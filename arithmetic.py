46d06c6546e78e9bc6e0cd00b74b051c5ecb9918
Arjav Sharma <arjav.sharma.91@gmail.com>
2026-06-05 07:48:58 -0400

    Update arithmetic.py

--- arithmetic.py
+++ arithmetic.py
@@ -1,6 +1,6 @@
 from .interval import Interval
 from .rounding import add_down, add_up, sub_down, sub_up, div_down, div_up, mult_down, mult_up
-
+from gmpy2 import mpfr
 # Arithmetic layer rule:
 # never modify Interval class
 # never implement rounding here
