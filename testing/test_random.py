3f850b9ea19405aef3e72143c8bd6e17489fc847
Arjav Sharma <arjav.sharma.91@gmail.com>
2026-06-08 18:56:32 -0400

    Update test_random.py

--- testing/test_random.py
+++ testing/test_random.py
@@ -1,4 +1,5 @@
 from intervals.arithmetic import add, sub, mul, div
+from tests.utils import rand_interval_mixed, sample, assert_contains
 
 def test_add_containment():
   x = rand_interval_mixed()
@@ -11,8 +12,6 @@ def test_add_containment():
 
   assert_contains(result, a + b)
 
-from intervals.arithmetic import sub
-
 def test_sub_containment():
   x = rand_interval_mixed()
   y = rand_interval_mixed()
@@ -23,8 +22,6 @@ def test_sub_containment():
   result = sub(x, y)
 
   assert_contains(result, a - b)
-
-from intervals.arithmetic import mul
 
 def test_mul_containment():
   x = rand_interval_mixed()
