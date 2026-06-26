a6eb63de20f0cc9261b78b524a4048a48f5375de
Arjav Sharma <arjav.sharma.91@gmail.com>
2026-06-26 10:20:59 -0400

    Create __init__.py

---
+++ interval/__init__.py
@@ -0,0 +1,23 @@
+from .interval import Interval
+from .decorated_interval import DecoratedInterval
+from .decorations import Decoration
+
+# 2. Fully Tracked Core Arithmetic (Using your decorated module)
+from .decorated_arithmetic import add, sub, mul, div, reciprocal
+
+# 3. Tracked Elementary Functions 
+from .decorated_functions import (
+    exp, log, sqrt, pow_int, sign, 
+    sin, cos, tan, asin, acos, atan, 
+    sinh, cosh, tanh, asinh, acosh, atanh, 
+    abs, atan2, interval_min, interval_max, nth_root
+)
+
+__all__ = [
+    'Interval', 'DecoratedInterval', 'Decoration',
+    'add', 'sub', 'mul', 'div', 'reciprocal',
+    'exp', 'log', 'sqrt', 'pow_int', 'sign',
+    'sin', 'cos', 'tan', 'asin', 'acos', 'atan',
+    'sinh', 'cosh', 'tanh', 'asinh', 'acosh', 'atanh',
+    'abs', 'atan2', 'interval_min', 'interval_max', 'nth_root'
+]
